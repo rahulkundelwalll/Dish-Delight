3ab03c9f2c5a1be05424f4efaf069b9e7d6cdf85
rahulkundelwalll <pooransherrahul@gmail.com>
2024-01-19 02:25:45 +0530

    vercel fixing

--- server/index.js
+++ server/index.js
@@ -12,12 +12,7 @@ import paymentRouter from './Routes/paymentRoute.js';
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://dish-delight-r4g9.vercel.app');
-  res.header('Access-Control-Allow-Credentials', true);
-  // Other CORS headers as needed
-  next();
-});
+app.use(cors({ origin: ['http://localhost:3000', 'https://dish-delight-r4g9.vercel.app'], credentials: true }));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
